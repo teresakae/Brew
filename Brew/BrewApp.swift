09c70ed45ecf0dba8cc0248d8dbc735c95c76746
kae <kae>
2026-04-19 00:25:37 +0800

    Deleted content view

--- Brew/BrewApp.swift
+++ Brew/BrewApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct BrewApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TimerView()
         }
     }
 }
--- Brew/Model/BrewPhase.swift
+++ Brew/Model/BrewPhase.swift
@@ -6,7 +6,6 @@
 //
 
 import Foundation
-import pler
 struct BrewPhase: Identifiable {
     let id: UUID
     let name: String
